878491b6362d57e85729cc1f58427df440ec660d
AT727 <103963122+AT727@users.noreply.github.com>
2022-05-09 10:21:03 -0400

    Create RobotContainer.java

---
+++ codeUnfinished/RobotContainer.java
@@ -0,0 +1,12 @@
+class RobotContainer{
+  public String motors = "motors";
+  public String intake = "intake";
+  
+  void initMotors(){
+    System.out.println("initializing " + motors);
+}
+  
+  void initIntake(){
+    System.out.println("initializing " + intake);
+}
+}
